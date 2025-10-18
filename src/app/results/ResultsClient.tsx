a4aa1cca9634e27f6cbba36e16661beae879192a
Y K <yudhish27@gmail.com>
2025-10-18 16:46:19 +0530

    More fixing for mobile

--- src/app/results/ResultsClient.tsx
+++ src/app/results/ResultsClient.tsx
@@ -245,7 +245,7 @@ export default function Results() {
             <form className='flex flex-col gap-4 w-full font-mono text-sm space-y-8'>
               <div className='flex flex-col gap-2'>
                 <div className='overflow-x-auto overflow-y-hidden'>
-                  <div className="inline-block min-w-full">
+                  <div className="whitespace-nowrap inline-block min-w-full">
                     <p className='mb-2 tracking-[-.01em]'>
                       Key:
                     </p>
